ad34bb5d75a6630a11af2f1889df34d51f266517
kashish <kashish@protonmail.com>
2024-02-04 17:45:49 +0530

    Update download_segmented_video.py
    Added multi threading for faster downloads

--- download_segmented_video.py
+++ download_segmented_video.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-# Downloads and combines video segments from the web. You need to put segment's url in proper order in the "link" variable.
+# Downloads and combines video segments from the web. You need to put segment's url in proper order in the "link_prefix" and "link_suffix" variable.
 # Tested on Debian 10 (Buster). Must work on Debian and Ubuntu derivatives.
-# Dependencies: sudo apt install python3 wget ffmpeg
+# Dependencies: sudo apt install python3 python3-wget ffmpeg
 # Written by kashish sharma. Github link: https://github.com/skashish/pyprograms/blob/master/download_segmented_video.py
 
 import os
 import wget
+import threading
 
 directory = str(os.environ['HOME'])+'/Downloads/.temp'
 
@@ -19,13 +20,52 @@ segment_number = 1
 
 retry_count = 0
 
+link_prefix = ''
+link_suffix = ''
+
 while(True):
     try:
-        link = 'https://cdn13.com/720p.h264.mp4/seg-' + str(segment_number) + '-v1-a1.ts?cdn_creation_time=15969&cdn_ttl=14456&cdn_cv_data=185.278.20.292&cdn_hash=7d801ca4868960a54297dd7853cf8c9'
-        print("Downloading segement "+ str(segment_number))
-        wget.download(link)
-        print()
-        segment_number = segment_number + 1
+        link1 = link_prefix + str(segment_number) + link_suffix
+        link2 = link_prefix + str(segment_number + 1) + link_suffix
+        link3 = link_prefix + str(segment_number + 2) + link_suffix
+        link4 = link_prefix + str(segment_number + 3) + link_suffix
+
+        def task1():
+            print("Downloading segement "+ str(segment_number))
+            wget.download(link1)
+            print()
+        def task2():
+            print("Downloading segement "+ str(segment_number + 1))
+            wget.download(link2)
+            print()
+        def task3():
+            print("Downloading segement "+ str(segment_number + 2))
+            wget.download(link3)
+            print()
+        def task4():
+            print("Downloading segement "+ str(segment_number + 3))
+            wget.download(link4)
+            print()
+        
+        # Create threads
+        thread1 = threading.Thread(target=task1)
+        thread2 = threading.Thread(target=task2)
+        thread3 = threading.Thread(target=task3)
+        thread4 = threading.Thread(target=task4)
+
+        # Start threads
+        thread1.start()
+        thread2.start()
+        thread3.start()
+        thread4.start()
+
+        # Wait for both threads to finish
+        thread1.join()
+        thread2.join()
+        thread3.join()
+        thread4.join()
+
+        segment_number = segment_number + 4
         retry_count = 0
         continue
     except:
@@ -36,6 +76,7 @@ while(True):
         else:
             print('Perhaps '+ str(segment_number-1) + ' was the last segment.')
             break
+
 
 # Join all the segments.
 
